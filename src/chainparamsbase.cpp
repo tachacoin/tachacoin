547fa7bb72fef5207f79af904579e01c057ccfe9
tachacoin <aphunedirin@gmail.com>
2019-10-26 01:48:01 +0200

    tacha

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -35,13 +35,13 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 3889);
+        return MakeUnique<CBaseChainParams>("", 45551);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet3", 13889);
+        return MakeUnique<CBaseChainParams>("testnet3", 45561);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 13889);
+        return MakeUnique<CBaseChainParams>("regtest", 45571);
     else if (chain == CBaseChainParams::UNITTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 13889);
+        return MakeUnique<CBaseChainParams>("regtest", 45572);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
